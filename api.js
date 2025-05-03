7d83b9bd3dbfe1bfe876b4b7ae969210fe7852fd
Aderson Jônatas <138100457+Aderson79@users.noreply.github.com>
2025-05-02 22:22:31 -0300

    Update api.js

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 const SECRET = "seuSegredoSuperSeguro"; // Use variável de ambiente para segurança
 
